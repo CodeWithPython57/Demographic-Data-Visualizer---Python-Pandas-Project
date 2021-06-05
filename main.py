c9061e0769549df4c580e5605b0d86b776ac40c8
CodeWithPython57 <68240991+CodeWithPython57@users.noreply.github.com>
2021-06-05 16:06:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import demographic_analyzer
+from unittest import main
+
+# Test your function by calling it here
+demographic_analyzer.calculate_demographic_data()
+# Run unit tests automatically
+main(module='test_module', exit=False)
